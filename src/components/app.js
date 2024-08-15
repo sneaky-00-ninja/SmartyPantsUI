c4ef68f49a7b402649d1403956d2f5728a245e58
sneaky-00-ninja <denmcn@yahoo.com>
2024-08-15 17:43:37 +0200

    Generated project

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>
+          LOGO
+        </div>
+        <h1>Smarty</h1>
+        <h2>Pants</h2>
+      </div>
+    );
+  }
+}
